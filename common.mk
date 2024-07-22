5ee50b32b7fc13a343fc5d7e03055eaf2f45283d
cwu18 <cwu18@arizona.edu>
2024-07-21 21:47:53 -0700

    Download corrfunc

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ CLINK ?=
 ## Set the python command (supply the full path to python you want to
 ## use, if different from directly calling `python` on the shell,
 ## as can be the case if python is set via an alias)
-PYTHON:=python
+PYTHON:=/home/cwu18/miniconda3/envs/dynhalo/bin/python
 
 ## Important note -> if you directly call /some/path/to/python
 ## then the previous two variables will be updated to point
